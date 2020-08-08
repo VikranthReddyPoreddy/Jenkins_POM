74fe65989df07e6123c07bd79986a63a482bd0f3
VikranthReddyPoreddy <vikky123reddy@gmail.com>
2020-08-08 21:28:59 +0530

    Second Commit message

---
+++ src/main/java/Sample1/JenkinsSampleProject/SampleProject_3.java
@@ -0,0 +1,12 @@
+package Sample1.JenkinsSampleProject;
+
+import org.testng.annotations.Test;
+
+public class SampleProject_3 
+{
+	@Test
+	public void m3()
+	{
+		System.out.println("method in class : SampleProject_3");
+	}
+}
