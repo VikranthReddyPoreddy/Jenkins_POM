fb185fcc894a9dfae46d7d3df4bcfb7415adccf7
VikranthReddyPoreddy <vikky123reddy@gmail.com>
2020-08-08 20:57:11 +0530

    fisrt sample commit

---
+++ src/main/java/Sample1/JenkinsSampleProject/SampleProject_2.java
@@ -0,0 +1,12 @@
+package Sample1.JenkinsSampleProject;
+
+import org.testng.annotations.Test;
+
+public class SampleProject_2 
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("method in class : SampleProject_2");
+	}
+}
